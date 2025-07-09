371096c88b5c807d987cab28b771f08305e2e745
Farjad <farjadkhalil7@gmail.com>
2025-07-09 21:47:38 +0500

    Initial setup

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,59 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { View } from "react-native";
+import { COLORS } from "../../constants/colors";
+
+export default function RootLayout() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          headerShown:false,
+          tabBarActiveTintColor:COLORS.primary,
+          tabBarInactiveTintColor:COLORS.textLight,
+          tabBarStyle:{
+            backgroundColor:COLORS.white,
+            borderTopColor:COLORS.border,
+            borderTopWidth:1,
+            paddingBottom:8,
+            paddingTop:8,
+            height:70
+          },
+          tabBarLabelStyle:{
+            fontSize:14,
+            fontWeight:600,
+          }
+
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Recipes",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="restaurant" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: "Search",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="search" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="favorites"
+          options={{
+            title: "Favorites",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="heart" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </View>
+  );
+}
